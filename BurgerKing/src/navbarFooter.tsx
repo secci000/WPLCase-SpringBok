0543242d098ab956694d6eb972d289b1a271e905
secci000 <s144124@ap.be>
2025-10-16 15:05:40 +0000

    all items next to eachother slightly incorrect positions

--- BurgerKing/src/navbarFooter.tsx
+++ BurgerKing/src/navbarFooter.tsx
@@ -5,27 +5,49 @@ import iconHamburgerMenu from './assets/iconHamburgermenu.webp'
 function NavbarFooter() {
   return (
     <>
-      <ul className='navbar'>
-        <img src={iconHamburgerMenu} alt="iconHamburgerMenu" />
-        <section className='section-logo'>
-        <img src={logoBK} alt="logoBK" />
-        </section>
-        <li className="dropdown">
-          <a href="#" className="dropbtn">NL</a>
-          <div className="dropdown-content">
-            <a href="#">NL</a>
-            <a href="#">FR</a>
-          </div>
-        </li>
-        <li><a href="#myBurgerKing">MyBurgerKing</a></li>
-        <li><a href="#jobs">Jobs</a></li>
-        <li><a href="#restaurants">Restaurants</a></li>
-        <li><a href="#deals">Deals</a></li>
-        <li><a href="#menu">Menu</a></li>
+      <nav>
+        <ul className='navbar'>
+          <li className='nav-hamburger'><img src={iconHamburgerMenu} alt="menu" /></li>
+          <li className='nav-logo'><img src={logoBK} alt="logo" /></li>
+          <li className='nav-pages'>
+            <a href="#">Menu</a>
+            <a href="#">Deals</a>
+            <a href="#">Restaurants</a>
+            <a href="#">Jobs</a>
+            <a href="#" className='nav-myBK'>MyBurgerKing</a>
+          </li>
+          <li  className="dropdown">
+            <a href="#" className="dropbtn">NL</a>
+            <div className="dropdown-content">
+              <a href="#">NL</a>
+              <a href="#">FR</a>
+            </div>
+          </li>
+        </ul>
 
-      </ul>
-      <h2>Dropdown Menu in Navbar</h2>
-      <p>Hover over the "Dropdown" link to see the dropdown menu.</p>
+      </nav>
+
+      <main>
+        <h1>MAIN</h1>
+      </main>
+      <footer>
+        <section className='footer-message'>Nuggets in je mailbox</section>
+        <section className='footer-signin-mailbox'>Schrijf je in voor de nieuwsbrief</section>
+        <section className='footer-links'>
+          <ul>
+            <li><a href="#">Contacteer ons</a></li>
+            <li><a href="#">Algemene gebruiksvoorwaarden</a></li>
+            <li><a href="#">Privacy en cookies</a></li>
+            <li><a href="#">Algemene voorwaarden Click&Collect en My Burger King</a></li>
+            <li><a href="#">Cookie-Instellingen</a></li>
+            <li><a href="#">IG</a></li>
+            <li><a href="#">X</a></li>
+            <li><a href="#">FB</a></li>
+            <li><a href="#">NL</a></li>
+
+          </ul>
+        </section>
+      </footer>
     </>
   )
 }
