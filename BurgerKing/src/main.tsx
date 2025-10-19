119445810244f29a1d238573feaf5c158df2893a
Matthew Wan <s144124@ap.be>
2025-10-19 05:00:52 +0000

    finished section download app

--- BurgerKing/src/main.tsx
+++ BurgerKing/src/main.tsx
@@ -1,19 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-<<<<<<< HEAD
-import "./index.css";
+import "./css/index.css";
 //import Menu from "./menu.tsx";
 import MyBK from "./myBK";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     {/*<Menu />*/}
     <MyBK/>
-=======
-import "./css/index.css";
-import Menu from "./menu.tsx";
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Menu />
->>>>>>> main
   </StrictMode>
 );
--- BurgerKing/src/myBK.tsx
+++ BurgerKing/src/myBK.tsx
@@ -2,13 +2,25 @@ import NavbarFooter from "./navbarFooter";
 import kingdomPhone from "./assets/myBK/kingdom-phone.webp";
 import appQR from "./assets/myBK/app-qr-code.webp";
 import arrow from "./assets/myBK/arrow.webp";
+import "./css/myBK.css"
 const MyBK = () => {
     return (
         <div>
             <NavbarFooter>
-                <section className="main-download-app">
-                    <h1>Download de app nu</h1>
-                    <p>Download de My Burger King app, spaar BK Coins, geniet en waan je in het koninkrijk der burgers.</p>
+                <section className="section-download-app">
+                    <article className="article-left">
+                        <img src={kingdomPhone} alt="kingdomPhone" />
+                    </article>
+                    <section className="section-right">
+                        <article className="article-qr-wrapper">
+                            <img className="img-qr"src={appQR} alt="QR" />
+                            <img className="img-arrow" src={arrow} alt="arrow" />
+                        </article>
+                        <article className="article-text-wrapper">
+                            <h1>Download de app nu</h1>
+                            <p>Download de My Burger King app, spaar BK Coins, geniet en waan je in het koninkrijk der burgers.</p>
+                        </article>
+                    </section>
                 </section>
             </NavbarFooter>
         </div>
