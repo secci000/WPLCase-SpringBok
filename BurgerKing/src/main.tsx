96f2bf77864f4e98f4600f13fb20cb08bd8df2a2
Matthew Wan <s144124@ap.be>
2025-10-19 04:04:59 +0000

    added images and app texts

--- BurgerKing/src/main.tsx
+++ BurgerKing/src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Menu from "./menu.tsx";
+//import Menu from "./menu.tsx";
+import MyBK from "./myBK";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Menu />
-  </StrictMode>,
+    {/*<Menu />*/}
+    <MyBK/>
+  </StrictMode>
 );
---
+++ BurgerKing/src/myBK.tsx
@@ -0,0 +1,18 @@
+import NavbarFooter from "./navbarFooter";
+import kingdomPhone from "./assets/myBK/kingdom-phone.webp";
+import appQR from "./assets/myBK/app-qr-code.webp";
+import arrow from "./assets/myBK/arrow.webp";
+const MyBK = () => {
+    return (
+        <div>
+            <NavbarFooter>
+                <section className="main-download-app">
+                    <h1>Download de app nu</h1>
+                    <p>Download de My Burger King app, spaar BK Coins, geniet en waan je in het koninkrijk der burgers.</p>
+                </section>
+            </NavbarFooter>
+        </div>
+    );
+};
+
+export default MyBK;
