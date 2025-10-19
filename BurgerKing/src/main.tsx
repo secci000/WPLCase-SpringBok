ce85a88a0294420d9355c80d8de2295a7033054b
Matthew Wan <s144124@ap.be>
2025-10-19 04:08:24 +0000

    added css folder and moved css files in there

--- BurgerKing/src/main.tsx
+++ BurgerKing/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./css/index.css";
 import Menu from "./menu.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
